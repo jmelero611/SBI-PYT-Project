7d09100338e96cb264b0055a63c7688da405e841
Lydia Fortea <lydia.fortea01@estudiant.upf.edu>
2018-04-02 19:44:02 +0200

    Add files via upload

--- final/modules/homodimers.py
+++ final/modules/homodimers.py
@@ -6,21 +6,28 @@ from Bio.PDB import *
 from Bio import pairwise2
 from common_functions import *
 import numpy
-import string
-import copy
+import sys
 
 #Function to build the complex for homodimers from all files
-def get_structure_homodimer(structure):
+def get_structure_homodimer(structure, option):
 	"""
-	Builds the structure when the input are homodimers.
-	Returns the complete predicted structure.
+	Superimpose the first homodimer interaction with the next ones to buil the new structure
+	 input: structure from which we want to perform all the superimposition
+	 output: structure with the corrected coordinates of the macrocomplex
 	"""
 	sup = Superimposer()
-	new_structure = structure.copy()
-	ref_model = new_structure[0]
+	structure = structure.copy()
+	new_structure = Structure.Structure("macrocomplex")
+	new_structure.add(Model.Model(0))
+	ref_model = structure[0]
+	new_structure[0].add(list(ref_model.get_chains())[0])
+	new_structure[0].add(list(ref_model.get_chains())[1])
+	n = 0
 
+	if option:
+		sys.stderr.write("Performing the superimposition with homodimer structures: \n")
 	#Iterate over all interactions
-	for alt_model in new_structure:
+	for alt_model in structure:
 		#Build paired lists of c-alpha atoms:
 		ref_atoms = []
 		alt_atoms = []
@@ -41,30 +48,42 @@ def get_structure_homodimer(structure):
 			assert numpy.max(numpy.abs(sup.rotran[0]) - numpy.identity(3)) < 0.000001
 
 		else :
-	        #Update the structure by moving all the atoms in
-	        #this model (not just the ones used for the alignment)
+	        #Update the structure by moving all the atoms in alt model
+	        #Delete the first chains used for superimposition
 			sup.apply(alt_model.get_atoms())
-			alt_model.detach_child(list(alt_model.get_chains())[0].id)
+			#Add chain if there is no clashes
 
-		print("RMS(first model, model %i) = %0.2f" % (alt_model.id, sup.rms))
+			add_chain = list(alt_model.get_chains())[1]
+			if not clash_identifier(new_structure, add_chain):
+				if option:
+					sys.stderr.write("\tRMSD of the homodimer reference and model %i is %0.2f\n" % (alt_model.id, sup.rms))
+				try:
+					new_structure[n].add(add_chain)
+				except:
+					n += 1
+					new_structure.add(Model.Model(n))
+					new_structure[n].add(add_chain)
+
 
 	return new_structure
 
 
 #Function to build the complex for homodimers from n interactions
-def get_structure_mix(all_interactions, homodimers, heterodimers):
+def get_structure_homodimer_heterodimer(all_interactions, homodimers, heterodimers):
 	"""
 	Predict the macrocomplex fromed by heterodimers and homodimers.
 	Superposes the homodimer interactions to get the right coordinates of the chains.
 	Adds the heterodimers interactions within the complex
-	Builds and returns the new structure.
+	Builds and returns the new structure without classes.
 	 input: two dictionaries with the structures from both homodimers and heterodimers interactins respectively.
 	 ouput: new structure formed by those interactions.
 	"""
 
-	super_imposer = Superimposer()
+	sup = Superimposer()
 	sup_chains = {}
 
+	if option:
+		sys.stderr.write("Performing the Superimposition and Alignment for a complex structure: \n")
 	#Get homodimer chains
 	n = 0
 	for str_id, str in homodimers.items():
@@ -76,7 +95,7 @@ def get_structure_mix(all_interactions, homodimers, heterodimers):
 		for alt_model in chains[1:]:
 			ref_atoms = []
 			alt_atoms = []
-			print("Comparing %s with %s" %(ref_model, alt_model))
+		
 			for (ref_chain, alt_chain) in zip(ref_model, alt_model):
 				for ref_res, alt_res in zip(ref_chain, alt_chain):
 					if ref_res.resname == alt_res.resname and ref_res.id == alt_res.id:
@@ -84,16 +103,20 @@ def get_structure_mix(all_interactions, homodimers, heterodimers):
 						alt_atoms.append(alt_res['CA'])
 
 			#Align these paired atom lists:
-			super_imposer.set_atoms(ref_atoms, alt_atoms)
+			sup.set_atoms(ref_atoms, alt_atoms)
 
 		    #Update the structure by moving all the atoms in
 		    #this model (not just the ones used for the alignment)
 			super_imposer.apply(list(alt_model[0].get_atoms()) + list(alt_model[1].get_atoms()))
+
 			sup_chains[n].append(ref_model[1])
 
-			print("RMS(ref %s, model %s) = %0.2f" % (ref_model, alt_model, super_imposer.rms))
+			if options:
+				sys.stderr.write("\tRMSD of the homodimer reference %s and model %s is %0.2f\n" % (str_id, model.id, sup.rms))
+			
 		n += 1
 	
+
 	#Add heterodimer interactions
 	for str_id, str in heterodimers.items():
 		n += 1
@@ -101,25 +124,30 @@ def get_structure_mix(all_interactions, homodimers, heterodimers):
 		alt_model = chains[0]
 		seq1 = get_sequence(alt_model[0])
 		seq2 = get_sequence(alt_model[1])
+
 		for n_inter in sup_chains:
 			ref_chain = sup_chains[n_inter][0]
 			ref_seq = get_sequence(ref_chain)
 			if (seq_comparison(ref_seq, seq1)):
 				ref_atoms = list(alt_model[0].get_atoms())
 				alt_atoms = list(ref_chain.get_atoms())
-				super_imposer.set_atoms(ref_atoms, alt_atoms)
-				super_imposer.apply(list(alt_model[0].get_atoms()) + list(alt_model[1].get_atoms()))
+				sup.set_atoms(ref_atoms, alt_atoms)
+				sup.apply(list(alt_model[0].get_atoms()) + list(alt_model[1].get_atoms()))
 				sup_chains[n] = [alt_model[1]]
-				print("RMS(ref %s, model %s) = %0.2f" % (chain1, chain, super_imposer.rms))
+
+				if options:
+					sys.stderr.write("\tRMSD of the reference %s and model %s is %0.2f\n" % (ref_chain, str_id, sup.rms))
+	
 				break
 
 			elif seq_comparison(ref_seq, seq2):
 				ref_atoms = list(alt_model[1].get_atoms())
 				alt_atoms = list(ref_chain.get_atoms())
-				super_imposer.set_atoms(ref_atoms, alt_atoms)
-				super_imposer.apply(list(alt_model[0].get_atoms()) + list(alt_model[1].get_atoms()))
+				sup.set_atoms(ref_atoms, alt_atoms)
+				sup.apply(list(alt_model[0].get_atoms()) + list(alt_model[1].get_atoms()))
 				sup_chains[n] = [alt_model[0]] 
-				print("RMS(ref %s, model %s) = %0.2f" % (alt_model, ref_chain, super_imposer.rms))
+				if options:
+					sys.stderr.write("\tRMSD of the reference %s and model %s is %0.2f\n" % (ref_chain, str_id, sup.rms))
 				break
 			else:
 				continue
@@ -132,14 +160,15 @@ def get_structure_mix(all_interactions, homodimers, heterodimers):
 	n = 0
 	for chains in sup_chains.values():
 		for chain in chains:
-			try:
-				new_structure[0].add(chain)
-			except:
-				n += 1
-				new_structure.add(Model.Model(n))
+			if bool(list(new_structure.get_chains())):
+				if not clash_identifier(new_structure, chain):
+					try:
+						new_structure[n].add(chain)
+					except:
+						n += 1
+						new_structure.add(Model.Model(n))
+						new_structure[n].add(chain)
+			else:
 				new_structure[n].add(chain)
 
 	return new_structure
-
-
-
