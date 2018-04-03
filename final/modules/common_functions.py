52fe8e3e564563059f2bf9cda7f604dfc3a0a16b
Lydia Fortea <lydia.fortea01@estudiant.upf.edu>
2018-04-03 17:30:00 +0200

    Add files via upload

--- final/modules/common_functions.py
+++ final/modules/common_functions.py
@@ -10,9 +10,12 @@ def get_sequence(chain):
 	"""
 	Deletes heteroatoms and returns the sequence of proteins with more than 30 aminoacids from their alpha carbons. 
 	It uses CaPPBuilder package from Bio.PDB.
+	 input: chain from which we want to get the sequence
+	 output: sequence object of the chain
 	""" 
 	ppb = CaPPBuilder()
 	residues_to_remove = []
+
 	for residue in chain:
 		if residue.id[0] != ' ':
 			residues_to_remove.append(residue.id) #select heteroatoms
@@ -21,7 +24,7 @@ def get_sequence(chain):
 		chain.detach_child(residue) #remove heteroatoms
 
 	if len(chain) <= 30:
-		return None	#select chains are suitable to be protein chains
+		return None	#select chains that are suitable to be protein chains
 	else:
 		pp = ppb.build_peptides(chain)
 		seq = pp[0].get_sequence()
@@ -33,34 +36,89 @@ def seq_comparison(seq1, seq2):
 	"""
 	Uses pariwise alignment to align a pair of sequences to find if two sequences are similar or not.
 	Returns True if percentage of identity is greater than 95%, and False if it is not.
+	 input: both sequences we want to align
+	 output True or False dependend of the percentage od identity
 	"""
 	alignment = pairwise2.align.globalxx(seq1, seq2)
 	score = alignment[0][2]
 	ident_perc = score / max(len(seq1), len(seq2))
-	#print("%s and %s has a identity of %f" %(seq1, seq2, ident_perc))
 
 	if ident_perc > 0.99:
 		return True
 	else:
 		return False
 
+
+#Compare chains from two structures
+def chains_comparison(str1, str2):
+	"""
+	Compares both chains from the input structures to know if they are similar or not
+	 input: both structures from which we want to perform the chains_comparison
+	 output: True if both structures have the same chains and False if they are differents
+	"""
+	rs = 0
+	rs_tot = [[0,0],[0,0]]
+
+	for chain1 in str1.get_chains():
+		seq1 = get_sequence(chain1)
+		for chain2 in str2.get_chains():
+			seq2 = get_sequence(chain2)
+			rs2 = 0
+			
+			if seq_comparison(seq1, seq2) is True:
+				rs_tot[rs][rs2] = 1
+			else:
+				rs_tot[rs][rs2] = 0
+		rs += 1
+
+	if 1 in rs_tot[0] and 1 in rs_tot[1]:
+		return True
+	else:
+		return False
+
+#Create temporal structure
+def temp_structure(interaction_dic, name):
+	"""
+	Builds a temporary structure and fill it with the interactions.
+	 input: dictionary with all pairwise interaction chains and the name of the structure
+	 output: new structure
+	"""
+	
+	new_structure = Structure.Structure(name)
+
+	i = 0
+	for interaction in interaction_dic.values():
+		for inter in interaction:
+			new_structure.add(Model.Model(i))
+			new_structure[i].add(inter[0])
+			new_structure[i].add(inter[1])
+			i += 1
+
+	return new_structure
+
 #Clash identifier
-def clash_identifier(structure):
+def clash_identifier(structure, add_chain):
 	"""
 	Check if there is a clash within a structure
 	Returns the number of clashes and if there is no clash, prints it.
-	 inputs: structure we want to evaluate
+	 inputs: structure we want to evaluate, chain we want to add
 	 output: number of clashes
 	"""
 
-	clash = 0
 	ns = NeighborSearch(list(structure.get_atoms()))
-	for chain in structure.get_chains():
-		for atom in chain.get_atoms():
-			for chain2 in ns.search(atom.get_coord(), 2, level='C'):
-				if chain2 == chain:
-					clash += 1
-	if clash == 0:
-		print("There is no clash")
-	return clash
 
+	for atom in add_chain.get_atoms():
+		for chain in ns.search(atom.get_coord(), 1, level='C'):
+			return True
+
+	return False
+
+#Save structure into a new pdb file
+def save_complex(structure, outfile):
+	"""Saves the structure into a PDB file. Uses Bio.PBIO package.
+	 input: structure from which we want to create a pdb file and the name of the file.
+	 ouput: new pdb file.
+	"""
+	io = PDBIO()
+	io.set_structure(structure)
+	io.save(outfile)
--- final/modules/heterodimers.py
+++ final/modules/heterodimers.py
@@ -7,14 +7,13 @@
 #Simple input file with iterations between heterodimers
 from Bio.PDB import *
 from Bio import pairwise2
-from Bio.SubsMat import MatrixInfo as matlist
-import string
+import sys
 from common_functions import *
 
 ###################### FUNCTIONS ########################
 
 ##Function to align sequences from chains
-def align_sequences_heterodimers(structure):
+def align_sequences_heterodimers(structure, option):
 	"""
 	Align all sequences generated from PDB files and returns the best alignments.
 	 input: structure from which we want to perform the alignments
@@ -25,6 +24,8 @@ def align_sequences_heterodimers(structure):
 	best_aln = []
 	n = len(chains)
 
+	if option:
+		sys.stderr.write("Align chain sequences: \n")
 	#Select sequences to align
 	for i in range(n-1):
 		id1 = chains[i].get_full_id()[1:]
@@ -33,40 +34,75 @@ def align_sequences_heterodimers(structure):
 		for j in range(i+1,n):
 			id2 = chains[j].get_full_id()[1:]
 			seq2 = get_sequence(chains[j])	
-			#print("align %s with %s" %(id1, id2))
+
 			#Align sequence
 			alns = pairwise2.align.globalxx(seq1, seq2)
 			score = alns[0][2]
 
 			#Calculate sequence identity
 			ident_perc = score / max(len(seq1), len(seq2))
-			#print(ident_perc)
+
 			if ident_perc > 0.99:
-				print("Sequence of %s has aligned with %s with a identity of %d\n" %(id1, id2, ident_perc))
 				best_aln.append((id1,id2))
+				if option:
+					sys.stderr.write("\t-Sequence of %s has aligned with %s with a identity of %d\n" %(id1, id2, ident_perc))
 	
 	return best_aln
 
 ## Function to superimpose sequences aligned
-def superimpose_structures_heterodimers(structure, best_aln):
+def superimpose_structures_heterodimers(structure, best_aln, option):
 	"""
 	Superimposes the best alignments from the input structures, gets the chains to create the new structure and returns the new structure superimposed.
 	 input: structure with all the chains and a list with the chains we want to superimpose
 	 output: structure with the correct coordinates of the macrocomplex
 	"""
 	sup = Superimposer()
-	new_structure = structure.copy()
+	structure = structure.copy()
+	new_structure = Structure.Structure("macrocomplex")
+	new_structure.add(Model.Model(0))
+	n = 0
+
+	if option:
+		sys.stderr.write("Performing the superimposition with heterodimer structure with best alignments: \n")
 
 	#get fixed and moving atoms from best alginments
 	for aln1, aln2 in best_aln:
-		ref_atoms = list(new_structure[aln1[0]][aln1[1]].get_atoms())
-		alt_atoms = list(new_structure[aln2[0]][aln2[1]].get_atoms())
+		ref_atoms = []
+		alt_atoms = []
+		for ref_res, alt_res in zip(structure[aln1[0]][aln1[1]], structure[aln2[0]][aln2[1]]):
+			if ref_res.resname == alt_res.resname and ref_res.id == alt_res.id:
+				ref_atoms.append(ref_res['CA'])
+				alt_atoms.append(alt_res['CA'])
 
 		#Align these paired atoms
 		sup.set_atoms(ref_atoms, alt_atoms) 
-		sup.apply(new_structure[aln2[0]].get_atoms())
-		new_structure[aln1[0]].detach_child(aln1[1])
-		print("RMS(model %s, model %s) = %0.2f" % (aln1, aln2, sup.rms))
-		
-	return new_structure
+		sup.apply(structure[aln2[0]].get_atoms())
 
+		#Add chain if there is no clashes
+		#add_chain = structure[aln2[0]][aln2[1]]
+		add_chain = structure[aln2[0]][aln2[1]]
+
+		if bool(list(new_structure.get_chains())):
+				if not clash_identifier(new_structure, add_chain):
+					if option:
+						sys.stderr.write("\t-RMSD of the model %s with respect model %s is %0.2f\n" % (aln1, aln2, sup.rms))
+					try:
+						new_structure[n].add(add_chain)
+					except:
+						n += 1
+						new_structure[n].add(add_chain)
+		else:
+			if len(best_aln) == 1:
+				for chain in structure[aln1[0]]:
+					new_structure[n].add(chain)
+				for chain in structure[aln2[0]]:
+					if chain.id != add_chain.id:
+						new_structure[n].add(chain)
+			
+			else:
+				new_structure[n].add(add_chain)
+	
+			if option:
+				sys.stderr.write("\t-RMSD of the model %s with respect model %s is %0.2f\n" % (aln1, aln2, sup.rms))
+
+	return new_structure
